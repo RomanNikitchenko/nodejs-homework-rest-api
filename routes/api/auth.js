4a17791ee648ece2c276ddca7b91917ced642dce
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-07-25 16:30:11 +0300

    ht4

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const controllers = require('../../controllers/contacts');
+
+const router = express.Router();
+
+router.post('/signup');
+
+module.exports = router;
