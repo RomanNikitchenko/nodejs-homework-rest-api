97b18646b291801b9fc0c9777cfe490ac7b99f50
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-08-02 15:25:04 +0300

    Jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,37 +1,41 @@
 const { User } = require('../../models/user');
-const path = require("path");
-const fs = require("fs/promises");
+const path = require('path');
+const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 //адрес к папке проекта в которую будем сохранять изображение
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars"); //C:\Users\User\Desktop\nodejs-homework-rest-api\public\avatars
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars'); //C:\Users\User\Desktop\nodejs-homework-rest-api\public\avatars
 
 const updateAvatar = async (req, res, next) => {
-    // req.file - объект мидлвары upload, имеет свойства path: и originalname:
-    const { path: tempUpload, originalname } = req.file; //переименовываем path: на tempUpload
+  // req.file - объект мидлвары upload, имеет свойства path: и originalname:
+  const { path: tempUpload, originalname } = req.file; //переименовываем path: на tempUpload
 
-    // req.user - объект мидлвары auth, имеет свойства _id:
-    const { _id: id } = req.user; //переименовываем _id: на id
+  // req.user - объект мидлвары auth, имеет свойства _id:
+  const { _id: id } = req.user; //переименовываем _id: на id
 
-    const imageName = `${id}_${originalname}`; //id пользователя_оригинальное имя файла(картинки)
+  const imageName = `${id}_${originalname}`; //id пользователя_оригинальное имя файла(картинки)
 
-    try {
-        const resultUpload = path.join(avatarsDir, imageName);
+  try {
+    const resultUpload = path.join(avatarsDir, imageName);
 
-        await fs.rename(tempUpload, resultUpload); //Метод fs.rename() используется для асинхронного переименования файла по указанному старому пути в заданный новый путь. Он перезапишет файл назначения, если он уже существует.
+    await fs.rename(tempUpload, resultUpload); //Метод fs.rename() используется для асинхронного переименования файла по указанному старому пути в заданный новый путь. Он перезапишет файл назначения, если он уже существует.
 
-        const avatarURL = path.join("public", "avatars", imageName);
+    const image = await Jimp.read(resultUpload);
+    image.resize(250, 250);
 
-        await User.findByIdAndUpdate(req.user._id, { avatarURL }); //в первом параметре находит объект по req.user._id во втором параметре что обновить у этого объекта
+    const avatarURL = path.join('public', 'avatars', imageName);
 
-        res.json({
-            ResponseBody: {
-                avatarURL,
-            }
-        });
-    } catch (error) {
-        await fs.unlink(tempUpload);
-        throw error;
-    }
+    await User.findByIdAndUpdate(req.user._id, { avatarURL }); //в первом параметре находит объект по req.user._id во втором параметре что обновить у этого объекта
+
+    res.json({
+      ResponseBody: {
+        avatarURL,
+      },
+    });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
 };
 
 module.exports = updateAvatar;
